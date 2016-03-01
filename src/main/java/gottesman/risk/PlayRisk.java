c3192b9eff0fe86346e11815e16e6d33c10e0dbd
Leba Gottesman <leebzgott@gmail.com>
2016-03-01 01:44:12 -0500

    Added Code

--- src/main/java/gottesman/risk/PlayRisk.java
+++ src/main/java/gottesman/risk/PlayRisk.java
@@ -1,6 +1,18 @@
 package gottesman.risk;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+
+//import sun.audio.AudioPlayer;
+//import sun.audio.AudioStream;
 
 public class PlayRisk extends JFrame {
 
@@ -8,7 +20,50 @@ public class PlayRisk extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private JButton button;
 
-	// TODO
+	public PlayRisk() throws IOException {
+
+		setTitle("Risk");
+		setSize(1000, 800);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setResizable(false);
+
+		Container container = getContentPane();
+		container.setLayout(new BorderLayout());
+		container.setBackground(Color.BLACK);
+		StartPanel panel = new StartPanel();
+		container.add(panel, BorderLayout.CENTER);
+
+		button = new JButton("PLAY GAME!");
+		button.setPreferredSize(new Dimension(150, 40));
+		container.add(button, BorderLayout.SOUTH);
+		button.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+
+				// dispose();
+				// new GameBoard().setVisible(true);
+
+			}
+		});
+
+		/*
+		 * InputStream in = new FileInputStream("risk music .wav");
+		 * 
+		 * AudioStream music = new AudioStream(in);
+		 * 
+		 * AudioPlayer.player.start(music);
+		 */
+	}
+
+	public static void main(String[] args) {
+		try {
+			new PlayRisk().setVisible(true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
---
+++ src/main/java/gottesman/risk/StartPanel.java
@@ -0,0 +1,40 @@
+package gottesman.risk;
+
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class StartPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private BufferedImage image;
+
+	public StartPanel() {
+
+		try {
+			image = ImageIO.read(new File("StartScreenPic.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		setLayout(new BorderLayout());
+
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+
+		super.paintComponent(g);
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+
+	}
+
+}
