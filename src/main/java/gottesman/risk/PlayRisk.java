1fa8777877468a3217c16a94168922e4905b0780
Leba Gottesman <leebzgott@gmail.com>
2016-03-17 00:27:20 -0400

    Modified Start Panel

--- src/main/java/gottesman/risk/PlayRisk.java
+++ src/main/java/gottesman/risk/PlayRisk.java
@@ -7,6 +7,7 @@ import gottesman.risk.map.MoveOrAttackController;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -37,7 +38,7 @@ public class PlayRisk extends JFrame implements GameStateListener {
 		setLocationRelativeTo(null);
 		setResizable(true);
 
-		Container container = getContentPane();
+		final Container container = getContentPane();
 		container.setLayout(new BorderLayout());
 
 		dataManager = new DataManager();
@@ -47,19 +48,31 @@ public class PlayRisk extends JFrame implements GameStateListener {
 		deployingController = new DeployingController(gameState);
 		moveOrAttackController = new MoveOrAttackController(gameState, dataManager);
 		fortifyController = new FortifyController(gameState, dataManager);
-
 		boardView.setGameController(deployingController);
 
-		container.add(boardView, BorderLayout.CENTER);
+		final StartPanel startPanel = new StartPanel();
+		container.add(startPanel, BorderLayout.CENTER);
+		final JButton startButton = new JButton("PLAY GAME!");
+		startButton.setPreferredSize(new Dimension(150, 40));
+		container.add(startButton, BorderLayout.NORTH);
+
+		startButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				startPanel.setVisible(false);
+				container.add(boardView, BorderLayout.CENTER);
+				container.add(phaseButton, BorderLayout.SOUTH);
+				startButton.setVisible(false);
+			}
+		});
 
 		phaseButton = new JButton();
 		phaseButton.setOpaque(true);
+		phaseButton.setPreferredSize(new Dimension(120, 30));
 		phaseButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				gameState.nextPhase();
 			}
 		});
-		container.add(phaseButton, BorderLayout.SOUTH);
 
 		gameState.startGame();
 
